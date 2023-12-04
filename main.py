a0db78b04dddd3e7e9304719f66fc1123cb634e8
pedromst2000 <40210465@esmad.ipp.pt>
2023-12-04 23:47:03 +0000

    init: added the init file for the project

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+Window = Tk()
+
+Window.title("PhotoShow")
+Window.geometry("1350x700")
+
+# to insert the icon on the window
+# Window.iconbitmap("assets/CraftingCook.ico")
+
+# remove the maximize button
+Window.resizable(0, 0)
+
+Window.mainloop()
