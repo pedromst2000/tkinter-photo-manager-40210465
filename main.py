2fe48aa73895c41249c76babf148dd16926d9d89
pedromst2000 <40210465@esmad.ipp.pt>
2023-12-11 02:30:18 +0000

    chore: add mainCanvas to Homepage

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ Window.iconbitmap("assets/PhotoShowIcon.ico")
 # remove the maximize button
 Window.resizable(0, 0)
 
+# canvas
+mainCanvas = Canvas(Window, width=1350, height=700)
+
+# to insert the image on the canvas
+mainImage = ImageTk.PhotoImage(Image.open("assets/images/Home_background.png"))
+
+# to insert the image on the canvas
+mainCanvas.create_image(0, 0, anchor=NW, image=mainImage)
+
+# to insert the canvas on the window with pack method
+mainCanvas.pack()
 
 
 Window.mainloop()
