1ea3d1eb7607bc007d71af0003a07e2aa8d97dac
pedromst2000 <40210465@esmad.ipp.pt>
2023-12-29 04:04:57 +0000

    refactor: fonts function

--- styles/fonts.py
+++ styles/fonts.py
@@ -1,9 +1,25 @@
 import tkinter.font as tkFont
 
 
-def quickSandRegular(size):
+def quickSandRegular(size: int) -> tkFont:
+    """
+    This function is used to set the font family to QuickSand Regular.
+
+    :param size: The size of the font.
+
+    :return: The font.
+    """
+
     return tkFont.Font(family="QuickSand", size=size, weight="normal")
 
 
-def quickSandBold(size):
+def quickSandBold(size: int) -> tkFont:
+    """
+    This function is used to set the font family to QuickSand Bold.
+
+    :param size: The size of the font.
+
+    :return: The font.
+    """
+
     return tkFont.Font(family="QuickSand", size=size, weight="bold")
