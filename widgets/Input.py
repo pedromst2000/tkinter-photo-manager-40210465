685b588a9a904f7cd4ed16402bbf6cf5ea271436
pedromst2000 <40210465@esmad.ipp.pt>
2023-12-29 04:05:13 +0000

    refactor: Input function widget

--- widgets/Input.py
+++ widgets/Input.py
@@ -3,38 +3,34 @@ from styles.fonts import *
 from styles.colors import *
 
 
-# inputText = Entry(
-#     width=30,
-#     borderwidth=0,
-#     font=quickSandRegular(14),
-#     background=colors["primary-50"],
-#     fg=colors["secondary-500"],
-#     highlightthickness=0,
-#     cursor="hand2",
-# )
-
-
 def _Input_(
+    Window: object,
     width: int,
     borderwidth: int,
     fontSize: str,
     placeX: int,
     placeY: int,
+    inputType: str,
 ) -> None:
     """
     This function is used to create a input and to be resuable along the interface was a widget.
 
     parameters:
 
+    Window: object
     width: int
     borderwidth: int
     fontSize: int
     placeX: int
     placeY: int
+    inputType: str
+
+    return: None
 
     """
 
     _Input_ = Entry(
+        master=Window,
         width=width,
         borderwidth=borderwidth,
         font=quickSandBold(fontSize),
@@ -47,16 +43,19 @@ def _Input_(
     _Input_.place(x=placeX, y=placeY)
 
     # on focus in event changing the background color and the foreground color
-    # background color = secondary-500 foreground color = primary- 50
-
     def on_focus_in(e):
         _Input_["background"] = colors["secondary-500"]
         _Input_["fg"] = colors["primary-50"]
+        _Input_.config(insertbackground=colors["primary-50"])
 
     def on_focus_out(e):
         _Input_["background"] = colors["secondary-300"]
         _Input_["fg"] = colors["secondary-500"]
+        _Input_.config(insertbackground=colors["secondary-500"])
 
     _Input_.bind("<FocusIn>", on_focus_in)
 
     _Input_.bind("<FocusOut>", on_focus_out)
+
+    if inputType == "password":
+        _Input_.config(show="*")
