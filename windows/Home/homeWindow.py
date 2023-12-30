18a0a1945f405bb6855d01de9f09d240526cb24e
pedromst2000 <40210465@esmad.ipp.pt>
2023-12-30 01:13:11 +0000

    refactor: Updates on the homeWindow

--- windows/Home/homeWindow.py
+++ windows/Home/homeWindow.py
@@ -1,7 +1,32 @@
-import tkinter as tk
+from tkinter import Tk
+from styles.colors import colors
+from models.users import getUserInfo
 
 
-def homeWindow():
+def homeWindow(email: str, isLogged: bool, isNewUser: bool) -> None:
+    """
+    This function is used to create the home window.
+
+    :param email: str
+    :param isLogged: bool
+    :param isNewUser: bool
+
+    :return: None
+
+    """
+
     # open the window
-    homeWindow = tk.Toplevel()
-    
+    _homeWindow_ = Tk()
+
+    # set the title
+    _homeWindow_.title("PhotoShow - Home")
+    _homeWindow_.iconbitmap("assets/PhotoShowIcon.ico")
+    _homeWindow_.resizable(0, 0)
+    _homeWindow_.geometry("1350x700")
+    _homeWindow_.config(bg=colors["primary-50"])
+
+    # # get the logged user payload
+    loggedUser = getUserInfo(email)
+    registerPayload = getUserInfo(email)  # get the user registered payload
+
+    _homeWindow_.mainloop()
