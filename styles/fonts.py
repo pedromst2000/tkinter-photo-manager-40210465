168b90562a51fa0115f6cda81ab73b1a88a1789d
pedromst2000 <40210465@esmad.ipp.pt>
2023-12-30 01:10:31 +0000

    refactor: Improved fonts styles

--- styles/fonts.py
+++ styles/fonts.py
@@ -23,3 +23,27 @@ def quickSandBold(size: int) -> tkFont:
     """
 
     return tkFont.Font(family="QuickSand", size=size, weight="bold")
+
+
+def quickSandRegularUnderline(size: int) -> tkFont:
+    """
+    This function is used to set the font family to QuickSand Regular Underline.
+
+    :param size: The size of the font.
+
+    :return: The font.
+    """
+
+    return tkFont.Font(family="QuickSand", size=size, weight="normal", underline=1)
+
+
+def quickSandBoldUnderline(size: int) -> tkFont:
+    """
+    This function is used to set the font family to QuickSand Bold Underline.
+
+    :param size: The size of the font.
+
+    :return: The font.
+    """
+
+    return tkFont.Font(family="QuickSand", size=size, weight="bold", underline=1)
