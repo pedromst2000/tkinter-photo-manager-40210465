f67f6c9a260412997e46b11c775dd4079a933a3c
pedromst2000 <40210465@esmad.ipp.pt>
2024-01-19 16:36:37 +0000

    refactor: Add missing atribute to a Class

--- classes/Category.py
+++ classes/Category.py
@@ -3,11 +3,13 @@ from database import Database
 
 class Category:
     # attributes
+    categoryID = 0
     category = ""
 
     # constructor
     def __init__(
         self,
+        categoryID: int = 0,
         category: str = "",
     ) -> None:
         """
@@ -21,9 +23,31 @@ class Category:
         :return: None
         """
 
+        self.categoryID = categoryID
         self.category = category
 
     # methods
+
+    def get_categories(self) -> list:
+        """
+        Method to get all instances of the Class Category
+
+        :return: list
+
+        """
+
+        db = Database(
+            users=[],
+            categories=[],
+            photos=[],
+            comments=[],
+            albuns=[],
+            favorites=[],
+            contacts=[],
+        )
+        categories = db.get_categories()
+        return categories
+
     def add_category(self):
         """
         Method to add new instance of the Class Category
